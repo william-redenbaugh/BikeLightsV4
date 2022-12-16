ab40b8ea72bb8f1355b19f147f7a932423f85dde
William Redenbaugh <wmredenbaugh@gmail.com>
2022-12-15 19:39:56 -0800

    added gitignore

--- particle_impl/BikeLightsParticleSparkles/src/led_matrix_mod/led_matrix_thread.cpp
+++ particle_impl/BikeLightsParticleSparkles/src/led_matrix_mod/led_matrix_thread.cpp
@@ -3,19 +3,18 @@
 #include "LedMatrix.h"
 #include "spark_wiring_system.h"
 
+#define NUMBER_OF_DEVICES 6
+#define CS_PIN A5
+LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
+
 void led_thread_init(void){
     ledMatrix.init();
     ledMatrix.setText("MAX7219 Animation Demo");
     ledMatrix.setNextText("Second text");
 }
 
-#define NUMBER_OF_DEVICES 6
-#define CS_PIN A5
-LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CS_PIN);
-
-static int x = 0;
 void led_matrix_thread(void *params){
-
+    int x = 0;
     for(;;){
         ledMatrix.clear();
         ledMatrix.scrollTextLeft();
